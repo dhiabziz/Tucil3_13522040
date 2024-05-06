0bd3b91ac0c944dcdec30e1cbfbc5a02c035625b
dhiabziz <dhiditabdi@gmail.com>
2024-05-06 16:31:19 +0700

    Updating UCS algorithm

--- src/UCS.java
+++ src/UCS.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 
 public class UCS {
-    public Result solve(String startword, String endword, Kamus kamus){
+    public static Result solve(String startword, String endword, Kamus kamus){
         PriorityQueue<Simpul> q = new PriorityQueue<>();
         HashSet<String> visited = new HashSet<>();
         q.add(new Simpul(startword));
@@ -12,24 +11,37 @@ public class UCS {
         while(!q.isEmpty()){
             Simpul currSimpul = q.remove();
             String currWord = currSimpul.getWord();
-            visited.add(currWord);
 
             count++;
-
-            if(currWord == endword){
+            if(currWord.equals(endword)){
+                System.out.println("Reach result!");
                 return new Result(currSimpul.findPath(), count);
             }else{
                 for(int i = 0; i<currWord.length(); i++){
-                    for(char c = 'a'; c<= 'z'; c++){
+                    for(char c = 'A'; c<= 'Z'; c++){
                         String tetangga = currWord.substring(0, i) + c + currWord.substring(i+1);
+                        /*
+                        try{
+                            Thread.sleep(300);
+                        }catch(Exception e){
+                            System.out.println("Hehe");
+                        }
+                        */
                         if(kamus.isValid(tetangga) && !visited.contains(tetangga)){
+                            //System.out.print(tetangga);
+                            visited.add(tetangga);
                             q.add(new Simpul(tetangga, currSimpul.getCost() + 1, currSimpul));
                         }
                     }
                 }
+
+    
+
+
+
             }
         }
-
+        
         return new Result(null, count);
 
     }
