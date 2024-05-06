4b7754306a20cb646eaabccfb3d9abf425c0188b
dhiabziz <dhiditabdi@gmail.com>
2024-05-06 14:29:50 +0700

    Implementing Kamus on Solver

--- src/Solver.java
+++ src/Solver.java
@@ -3,7 +3,9 @@ public class Solver{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         Boolean stop = false;
-        String startword, endword;
+        String startword, endword, filepath;
+        Kamus kamus;
+
         while(!stop){
             System.out.println("Start word: ");
             startword = scanner.nextLine();
@@ -14,7 +16,16 @@ public class Solver{
                 System.out.println("\nLength of startword and endword is different!");
             }else{
                 stop = true;
-                System.out.println("Oke!");
+                filepath = "resources\\" +  startword.length() + "-letter-words.txt";
+                kamus = new Kamus(filepath);
+                System.out.println("Tes1");
+                String tes = "EAT";
+                if(kamus.isValid(tes)){
+                    System.out.println("Aman!");
+                }
+                else{
+                    System.out.println("Tes2");
+                }
             }
             
         }
